9217feb997a4c79525074b71b48fd18a2769d64a
Kartman1 <dimazhzh@gmail.com>
2024-12-19 14:06:43 +0200

    New command 3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("This is our second commit");
         System.out.println("new command");
         System.out.println("new command 2");
+        System.out.println("new command3");
     }
 }
