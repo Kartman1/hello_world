24ee965548c7ed2c7518fb4c89796eb88cdccc5f
Kartman1 <dimazhzh@gmail.com>
2024-12-24 02:03:14 +0200

    Обновляю файл в процессе обучения урока 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,45 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("This is our second commit");
-        System.out.println("new command");
-        System.out.println("new command 2");
-        System.out.println("new command3");
+        System.out.println("He llo World");
+        String hello = "Hello";
+        int a1 = 10;
+        char a2 = 'a';
+        byte a3 = 10;
+        short a4 = 32000;
+        long a5 = 1000L;
+        float a6 = 1.00f;
+        double a7 = 1.00;
 
-        System.out.println("new command4");
-        System.out.println("new command 5");
-        System.out.println("new command 6");
+/*        String name1 = "Igor";
+        String name2 = "Igore";
+        System.out.println(name1.equals(name2));
+        int lenght = name2.length();
+        System.out.println(lenght);
+        String firstName = "Elon";
+        String lastName = "Musk";
+        String fullname = firstName + " " + lastName + " 1996";
+        System.out.println(fullname);
+
+        String phrase = "Back to future";
+
+        System.out.println(phrase.toUpperCase());
+        System.out.println(phrase.toLowerCase());
+
+        String word = "back";
+        boolean phraseContains = phrase.contains("ta");
+        System.out.println(phraseContains);
+
+        System.out.println(phrase.toLowerCase().contains(word.toLowerCase()));
+
+        // join(Разделитель, строка1, строка2, строка3, ...) - Соединение строк
+        String joinString = String.join("", "Соединяем",  " Эти", " Строки");
+        System.out.println(joinString);
+
+        //
+*/
+
+        System.out.println(1f / 3f);
+        
+
     }
 }
