812b9e9035cf7845fade9885146d4f947203c382
Kartman1 <dimazhzh@gmail.com>
2025-01-02 23:24:06 +0200

    new class NameEncoderDecoder

---
+++ src/main/java/NameEncoderDecoder.java
@@ -0,0 +1,32 @@
+public class NameEncoderDecoder{
+    public String encode(String name){
+        String result = name
+                .replace("e", "1")
+                .replace("u", "2")
+                .replace("i", "3")
+                .replace("o", "4")
+                .replace("a", "5");
+        return "NOTFORYOU" + result + "YESNOTFORYOU";
+
+    }
+
+    public String decode(String name){
+        String result = name
+                .replace("1", "e")
+                .replace("2", "u")
+                .replace("3", "i")
+                .replace("4", "o")
+                .replace("5", "a");
+        result = result.replace("NOTFORYOU", "");
+        result = result.replaceAll("(?i)YESNOTFORYOU", "");
+        result = result.replace("YES", "");
+        return result;
+    }
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
