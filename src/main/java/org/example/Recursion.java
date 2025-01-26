52fb72e418b979c7b93fa50c44b91f34838c296b
Kartman1 <dimazhzh@gmail.com>
2025-01-26 14:39:04 +0200

    lesson4_10

--- src/main/java/org/example/Recursion.java
+++ src/main/java/org/example/Recursion.java
@@ -1,8 +1,14 @@
 package org.example;
+import java.util.regex.*;
 
 public class Recursion {
     public static void main(String[] args) {
+        String str = "Skoda 200";
+        String result;
         System.out.println(factorial(4));
+        System.out.println("intGet(\"Skoda 200\") = " + intGet(str));
+        result = str.replaceAll("[\\d\\s]+", "");
+        System.out.println("result = " + result);
     }
 
     public static int factorial(int number){
@@ -11,5 +17,17 @@ public class Recursion {
         }
         int result = number * factorial(number - 1);
         return result;
+    }
+
+    public static int intGet(String str){
+        int number = 0;
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(str);
+        if(matcher.find()){
+
+            number = Integer.parseInt(matcher.group());
+
+        }
+        return number;
     }
 }
