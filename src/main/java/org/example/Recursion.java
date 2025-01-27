20e70ea16e0421abdb3a6a0667886c25cda71ce2
Kartman1 <dimazhzh@gmail.com>
2025-01-27 02:05:55 +0200

    aa

--- src/main/java/org/example/Recursion.java
+++ src/main/java/org/example/Recursion.java
@@ -4,11 +4,15 @@ import java.util.regex.*;
 public class Recursion {
     public static void main(String[] args) {
         String str = "Skoda 200";
+        String str2 = "***";
+        String str3 = "";
+        str3 = str2.replace("*", "");
         String result;
         System.out.println(factorial(4));
         System.out.println("intGet(\"Skoda 200\") = " + intGet(str));
         result = str.replaceAll("[\\d\\s]+", "");
         System.out.println("result = " + result);
+        System.out.println("str3 = " + str3);
     }
 
     public static int factorial(int number){
